6bb2933740fd3a3d1c4c09703f9ec57f5771eccc
patelkomal23 <patelkomal2332@gmail.com>
2025-04-22 16:47:09 +0530

    done

--- array&Obj/index.js
+++ array&Obj/index.js
@@ -29,6 +29,45 @@
 
 
 //splice
-let array = [0,11,2,5,8,6];
-let arr = array.splice(1,3);
-console.log(arr);
+// let array = [0,11,2,5,8,6];
+// let arr = array.splice(1,3);
+// console.log(arr);
+
+// map
+// let array = [0,11,2,5,8,6];
+// array.map((val,index)=>{
+//     console.log(val);
+    
+// })
+
+
+//filter
+// let array = [0,11,2,5,8,6];
+// let arr = array.filter((val,index)=>{
+// if(val % 2 == 0) return val;   
+// })
+// console.log(arr);
+
+
+//spread operator
+// let arr1=[2,3,4,5,6,7,8];
+// let arr2=[6,7,5,33,4,5,6];
+
+// let array=[...arr1,...arr2];
+// console.log(array);
+
+// merging
+
+let obj1 = {
+    1 : "a",
+    2 : "b",
+    3 : "c"
+}
+let obj2 = {
+   4 : "a",
+    5: "b",
+    6: "c"
+
+}
+let obj= Object.assign(obj1, obj2);
+console.log(obj);
