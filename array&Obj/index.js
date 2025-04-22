277af11b8da7512389ada1ed88afd6a5e11c2b4d
patelkomal23 <patelkomal2332@gmail.com>
2025-04-22 16:27:10 +0530

    done

---
+++ array&Obj/index.js
@@ -0,0 +1,34 @@
+// array method
+
+//push
+// let array =[1,2,3,4,5];
+// array.push(6);
+// console.log(array);
+
+//pop
+// let array = [1,2,3,4,5];
+// array.pop();
+// console.log(array);
+
+//unshift
+// let array = [1,2,3,4,5,6];
+// array.unshift(7);
+// console.log(array);
+
+//shift
+
+// let array = [0,1,2,3,4,5];
+// array.shift();
+// console.log(array);
+
+//slice
+
+// let array = [0,1,2,3,4,5];
+// let arr=array.slice(0,3);
+// // console.log(arr);
+
+
+//splice
+let array = [0,11,2,5,8,6];
+let arr = array.splice(1,3);
+console.log(arr);
