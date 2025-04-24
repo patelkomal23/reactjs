109abd8504f1d6557637ad794a3d1870d6dad6e3
patelkomal23 <patelkomal2332@gmail.com>
2025-04-24 16:36:54 +0530

    done

--- Installation/react_app/src/App.jsx
+++ Installation/react_app/src/App.jsx
@@ -1,11 +1,12 @@
-import React ,{Fragment} from 'react'
+import React, { Fragment } from 'react'
 
 function App() {
   return (
-    <div>
+    <>
       <h1>komal</h1>
       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit iure ducimus fuga similique, illum tenetur nam magnam libero vel reiciendis quis maiores rem modi ex excepturi deleniti iusto quia ut.</p>
-    </div>
+    </>
+
   )
 }
 
