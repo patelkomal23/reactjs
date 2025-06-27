0700305e655f5cbb5c94234da33b3a3ff3b727a9
patelkomal23 <patelkomal2332@gmail.com>
2025-06-27 16:36:38 +0530

    done

--- axios-firebase/src/features/users/thunk.js
+++ axios-firebase/src/features/users/thunk.js
@@ -5,8 +5,8 @@ import { axiosInstance } from "../../axios/axiosApi";
 
 export const createUser = createAsyncThunk('user/createUser',async (user,{rejectWithValue})=>{
     try {
-        let res = await axiosInstance.post('/.json',user); // add data to firebase & ./json is a syntax must be return
-        return {...user, id : res.data.name} // add to userSlice : user [] <---
+        let res = await axiosInstance.post('/.json',user); 
+        return {...user, id : res.data.name} 
     } catch (error) {
         return rejectWithValue(error.message)
     }
